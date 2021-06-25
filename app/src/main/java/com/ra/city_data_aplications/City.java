82e5f0f495039da62ff2e4ae0dd858a65268272c
Ricardo Rodriguez <ricardo.3801910919@ucaldas.edu.co>
2021-06-25 13:05:37 -0500

    first commit

---
+++ app/src/main/java/com/ra/city_data_aplications/City.java
@@ -0,0 +1,69 @@
+package com.ra.city_data_aplications;
+
+public class City {
+    int id;
+    String nombre;
+    int poblacion;
+    double latitud;
+    double longitud;
+
+    public City() {
+    }
+
+    public City(int id, String nombre, int poblacion, double latitud, double longitud) {
+        this.id = id;
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\n" +
+                "Nombre: " + nombre + "\n" +
+                "Poblacion: " + poblacion + "\n" +
+                "Latitud: " + latitud + "\n" +
+                "Longitud: " + longitud;
+    }
+}
---
+++ app/src/main/java/com/ra/city_data_aplications/MainActivity.java
@@ -0,0 +1,87 @@
+package com.ra.city_data_aplications;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText txtId;
+    private EditText txtNombre;
+    private EditText txtPoblacion;
+    private EditText txtLatitud;
+    private EditText txtLongitud;
+    private Button btnGuardar;
+    private Button btnListar;
+    private ListView lvListar;
+    private ArrayAdapter<String> adapter;
+    MyDbCities db = new MyDbCities(this);
+    ArrayList<City> cities;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtId = findViewById(R.id.txtId);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtPoblacion = findViewById(R.id.txtPoblacion);
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLongitud = findViewById(R.id.txtLongitud);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        btnListar = findViewById(R.id.btnListar);
+        lvListar = findViewById(R.id.lvListar);
+        btnGuardar.setOnClickListener(this);
+        btnListar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnGuardar){
+            agregarCiudad();
+        }
+        if (v.getId() == R.id.btnListar){
+            cities = db.selectCity(db.getWritableDatabase());
+            ordenarAlfabeticamente();
+            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, cities);
+            lvListar.setAdapter(adapter);
+        }
+    }
+
+    private void agregarCiudad() {
+        int id = Integer.parseInt(txtId.getText().toString());
+        String nombre = txtNombre.getText().toString();
+        int poblacion = Integer.parseInt(txtPoblacion.getText().toString());
+        double latitud = Double.parseDouble(txtLatitud.getText().toString());
+        double longitud = Double.parseDouble(txtLongitud.getText().toString());
+
+        City city = new City(id, nombre, poblacion, latitud, longitud);
+        db.insertCity(db.getWritableDatabase(), city);
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        txtId.getText().clear();
+        txtNombre.getText().clear();
+        txtPoblacion.getText().clear();
+        txtLatitud.getText().clear();
+        txtLongitud.getText().clear();
+    }
+
+    private void ordenarAlfabeticamente() {
+        Collections.sort(cities, new Comparator<City>() {
+            @Override
+            public int compare(City c1, City c2) {
+                return c1.getNombre().compareTo(c2.getNombre());
+            }
+        });
+    }
+}
